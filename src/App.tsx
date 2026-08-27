630dc497b7bb29d8fc064de145029fe68963a467
Dyad <northnestbusiness@gmail.com>
2026-08-27 23:38:48 +0530

    Reverted all changes back to version 2dd99243af15ff06ee812f92488acca564851400

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Upload from "./pages/Upload";
-import Review from "./pages/Review";
-import Monthly from "./pages/Monthly";
-import SettingsPage from "./pages/Settings";
-import AppShell from "./components/AppShell";
-import { AppProvider } from "./store/AppContext";
 
 const queryClient = new QueryClient();
 
@@ -19,21 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <AppProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<AppShell />}>
-              <Route path="/" element={<Index />} />
-              <Route path="/upload" element={<Upload />} />
-              <Route path="/review/:reportId" element={<Review />} />
-              <Route path="/monthly" element={<Monthly />} />
-              <Route path="/settings" element={<SettingsPage />} />
-            </Route>
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
